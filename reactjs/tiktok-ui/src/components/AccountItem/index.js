878b4b4aec3945abef6eb41ae20370a548d409b5
Minh Duc <pmdphan@gmail.com>
2023-06-25 11:59:48 +0700

    #4

---
+++ reactjs/tiktok-ui/src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://haycafe.vn/wp-content/uploads/2022/03/Avatar-TikTok-anh-dai-dien-TikTok.jpg"
+                alt=""
+            />
+            <div className={cx('infor')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
---
+++ reactjs/tiktok-ui/src/components/Popper/Menu/MenuItem.js
@@ -0,0 +1,15 @@
+import styles from './Menu.module.scss';
+import Button from '~/components/Button';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+function MenuItem({ data }) {
+    return (
+        <Button className={cx('menu-item')} leftIcon={data.icon} to={data.to}>
+            {data.title}
+        </Button>
+    );
+}
+
+export default MenuItem;
